35efdcf8c95a531dfa6add7411fb81d8a5421d42
Kathirvelprakash <kathirvelprakash.m@gmail.com>
2025-03-18 17:44:04 +0530

    New files are Udated

--- Task_1.php
+++ Task_1.php
@@ -7,12 +7,25 @@
 $mark = $error = [];
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
+
+    if (empty($_POST['tam']) || empty($_POST['eng']) || empty($_POST['math']) || empty($_POST['sci']) || empty($_POST['soc'])) {
+        echo "One or more fields are empty!";
+    }elseif(!is_numeric($_POST['tam']) || !is_numeric($_POST['eng']) || !is_numeric($_POST['math']) || !is_numeric($_POST['sci']) || !is_numeric($_POST['soc'])){
+        echo "Enter the value in number format!";
+    }else{
         $mark[]=$_POST['tam'];
         $mark[]=$_POST['eng'];
         $mark[]=$_POST['math'];
         $mark[]=$_POST['sci'];
         $mark[]=$_POST['soc'];
-        print_r($mark);
+    }
+    
+    function calc($mark){
+        $total = $mark[0] + $mark[1] + $mark[2] + $mark[3] + $mark[4];
+        global $avg;
+        $avg = $total / 5;
+        echo "Average Marks: ".$avg."<br>";
+    }
 }
 ?>
 
@@ -33,4 +46,19 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     <input type="text" name="soc" value="<?php echo $no_stud ?? "" ;?>"> <br>
     <input type="submit" value="submit">
 </form>
+<?php
+    calc($mark);
+    $avg = 101;
+    if($avg<=100 && $avg>=90){
+        echo "Grade : A+";
+    }elseif($avg<=89 && $avg>=80){
+        echo "Grade : A";
+    }elseif($avg<=79 && $avg>=70){
+        echo "Grade : B+";
+    }elseif($avg<=69 && $avg>=60){
+        echo "Grade : B";
+    }else{
+        echo "Grade : Fail";
+    }
+?>
 </html>
