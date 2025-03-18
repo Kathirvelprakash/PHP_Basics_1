62c1c7963aa1d26e752db6e89e4bf2b448ae5675
Kathirvelprakash <kathirvelprakash.m@gmail.com>
2025-03-18 17:49:34 +0530

    New files are Udated

--- Task_1.php
+++ Task_1.php
@@ -3,62 +3,73 @@
 <body>
 
 <?php
-// $no_stud="";
-$mark = $error = [];
+$mark = [];
+$avg = null;
 
-if($_SERVER['REQUEST_METHOD']=="POST"){
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (
+        empty($_POST['tam']) || empty($_POST['eng']) || empty($_POST['math']) || 
+        empty($_POST['sci']) || empty($_POST['soc'])
+    ) {
+        echo "<div style='color:red;'>One or more fields are empty!</div>";
+    } elseif (
+        !is_numeric($_POST['tam']) || $_POST['tam'] < 0 || $_POST['tam'] > 100 ||
+        !is_numeric($_POST['eng']) || $_POST['eng'] < 0 || $_POST['eng'] > 100 ||
+        !is_numeric($_POST['math']) || $_POST['math'] < 0 || $_POST['math'] > 100 ||
+        !is_numeric($_POST['sci']) || $_POST['sci'] < 0 || $_POST['sci'] > 100 ||
+        !is_numeric($_POST['soc']) || $_POST['soc'] < 0 || $_POST['soc'] > 100
+    ) {
+        echo "<div style='color:red;'>Enter valid marks (0-100) only!</div>";
+    } else {
+        $mark = [
+            $_POST['tam'],
+            $_POST['eng'],
+            $_POST['math'],
+            $_POST['sci'],
+            $_POST['soc']
+        ];
 
-    if (empty($_POST['tam']) || empty($_POST['eng']) || empty($_POST['math']) || empty($_POST['sci']) || empty($_POST['soc'])) {
-        echo "One or more fields are empty!";
-    }elseif(!is_numeric($_POST['tam']) || !is_numeric($_POST['eng']) || !is_numeric($_POST['math']) || !is_numeric($_POST['sci']) || !is_numeric($_POST['soc'])){
-        echo "Enter the value in number format!";
-    }else{
-        $mark[]=$_POST['tam'];
-        $mark[]=$_POST['eng'];
-        $mark[]=$_POST['math'];
-        $mark[]=$_POST['sci'];
-        $mark[]=$_POST['soc'];
+        $avg = calc($mark);
     }
-    
-    function calc($mark){
-        $total = $mark[0] + $mark[1] + $mark[2] + $mark[3] + $mark[4];
-        global $avg;
-        $avg = $total / 5;
-        echo "Average Marks: ".$avg."<br>";
+}
+
+// Function to Calculate Average
+function calc($mark) {
+    $total = array_sum($mark);
+    $avg = $total / count($mark);
+    echo "<div>Average Marks: " . number_format($avg, 2) . "</div>";
+    return $avg;
+}
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+    <h1>Enter 5 Subjects Marks</h1>
+    Tamil: <input type="text" name="tam" value="<?php echo $_POST['tam'] ?? ""; ?>"> <br>
+    English: <input type="text" name="eng" value="<?php echo $_POST['eng'] ?? ""; ?>"> <br>
+    Maths: <input type="text" name="math" value="<?php echo $_POST['math'] ?? ""; ?>"> <br>
+    Science: <input type="text" name="sci" value="<?php echo $_POST['sci'] ?? ""; ?>"> <br>
+    Social: <input type="text" name="soc" value="<?php echo $_POST['soc'] ?? ""; ?>"> <br>
+    <input type="submit" value="Submit">
+</form>
+
+<?php
+// Display Grade if Average is Calculated
+if ($avg !== null) {
+    echo "<br><b>Grade: ";
+    if ($avg >= 90) {
+        echo "A+";
+    } elseif ($avg >= 80) {
+        echo "A";
+    } elseif ($avg >= 70) {
+        echo "B+";
+    } elseif ($avg >= 60) {
+        echo "B";
+    } else {
+        echo "Fail";
     }
+    echo "</b>";
 }
 ?>
 
 </body>
-
-<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-    <!-- <input type="text" name="no_stud" value="<?php echo $no_stud ?? "" ;?>"> <br> -->
-     <h1>Enter 5 Subjects Mark</h1>
-    Tamil : 
-    <input type="text" name="tam" value="<?php echo $no_stud ?? "" ;?>"> <br>
-    English : 
-    <input type="text" name="eng" value="<?php echo $no_stud ?? "" ;?>"> <br>
-    Maths : 
-    <input type="text" name="math" value="<?php echo $no_stud ?? "" ;?>"> <br>
-    Science : 
-    <input type="text" name="sci" value="<?php echo $no_stud ?? "" ;?>"> <br>
-    Social : 
-    <input type="text" name="soc" value="<?php echo $no_stud ?? "" ;?>"> <br>
-    <input type="submit" value="submit">
-</form>
-<?php
-    calc($mark);
-    $avg = 101;
-    if($avg<=100 && $avg>=90){
-        echo "Grade : A+";
-    }elseif($avg<=89 && $avg>=80){
-        echo "Grade : A";
-    }elseif($avg<=79 && $avg>=70){
-        echo "Grade : B+";
-    }elseif($avg<=69 && $avg>=60){
-        echo "Grade : B";
-    }else{
-        echo "Grade : Fail";
-    }
-?>
 </html>
