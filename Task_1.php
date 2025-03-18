89f4d7989fb237a0d48c5184943647686cac3f8b
Kathirvelprakash <kathirvelprakash.m@gmail.com>
2025-03-19 00:01:45 +0530

    New files are Udated

--- Task_1.php
+++ Task_1.php
@@ -7,17 +7,19 @@ $no_stud = $error = null;
 $marks = [];
 $averages = [];
 
+// If "Next" is clicked, store student count
 if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['nxt'])) {
     if (empty($_POST['no_stud'])) {
-        echo "<div style='color:red;'>Please enter the number of students!</div>";
+        $error = "<div style='color:red;'>Please enter the number of students!</div>";
     } elseif (!is_numeric($_POST['no_stud']) || $_POST['no_stud'] < 1) {
-        echo "<div style='color:red;'>Enter a valid positive number!</div>";
+        $error = "<div style='color:red;'>Enter a valid positive number!</div>";
     } else {
         $no_stud = (int)$_POST['no_stud'];
+        $_SESSION['no_stud'] = $no_stud; // Store student count in session
     }
 }
 
-// Handle marks submission
+// If "Submit" is clicked, store student marks
 if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['Submit'])) {
     $no_stud = $_POST['student_count'] ?? 0;
     
@@ -29,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['Submit'])) {
             'sci'  => $_POST["sci"][$i] ?? '',
             'soc'  => $_POST["soc"][$i] ?? ''
         ];
-
+        
         // Validate marks
         foreach ($marks[$i] as $subject => $score) {
             if (!is_numeric($score) || $score < 0 || $score > 100) {
@@ -52,36 +54,39 @@ function calc($mark) {
 }
 ?>
 
-<!-- Form to Get Number of Students -->
+<?php if (!isset($_POST['nxt']) && !isset($_POST['Submit'])): ?>
+<!-- Step 1: Enter Number of Students -->
 <form action="" method="post">
     <h3>Enter Number of Students</h3>
     <input type="text" name="no_stud" value="<?php echo htmlspecialchars($no_stud); ?>">
     <input type="submit" name="nxt" value="Next">
 </form>
-
-<?php if ($no_stud): ?>
-    <!-- Form to Enter Marks for Each Student -->
-    <form action="" method="post">
-        <input type="hidden" name="student_count" value="<?php echo $no_stud; ?>">
-        <h3>Enter Marks for <?php echo $no_stud; ?> Students</h3>
-        
-        <?php for ($i = 0; $i < $no_stud; $i++): ?>
-            <h4>Student <?php echo $i + 1; ?></h4>
-            Tamil: <input type="text" name="tam[]" value="<?php echo $_POST["tam"][$i] ?? ""; ?>"> <br>
-            English: <input type="text" name="eng[]" value="<?php echo $_POST["eng"][$i] ?? ""; ?>"> <br>
-            Maths: <input type="text" name="math[]" value="<?php echo $_POST["math"][$i] ?? ""; ?>"> <br>
-            Science: <input type="text" name="sci[]" value="<?php echo $_POST["sci"][$i] ?? ""; ?>"> <br>
-            Social: <input type="text" name="soc[]" value="<?php echo $_POST["soc"][$i] ?? ""; ?>"> <br><br>
-        <?php endfor; ?>
-        
-        <input type="submit" name="Submit" value="Submit">
-    </form>
 <?php endif; ?>
 
+<?php if (isset($_POST['nxt']) && empty($_POST['Submit'])): ?>
+<!-- Step 2: Enter Marks for Each Student -->
+<form action="" method="post">
+    <input type="hidden" name="student_count" value="<?php echo $no_stud; ?>">
+    <h3>Enter Marks for <?php echo $no_stud; ?> Students</h3>
+    
+    <?php for ($i = 0; $i < $no_stud; $i++): ?>
+        <h4>Student <?php echo $i + 1; ?></h4>
+        Tamil: <input type="text" name="tam[]" value="<?php echo $_POST["tam"][$i] ?? ""; ?>"> <br>
+        English: <input type="text" name="eng[]" value="<?php echo $_POST["eng"][$i] ?? ""; ?>"> <br>
+        Maths: <input type="text" name="math[]" value="<?php echo $_POST["math"][$i] ?? ""; ?>"> <br>
+        Science: <input type="text" name="sci[]" value="<?php echo $_POST["sci"][$i] ?? ""; ?>"> <br>
+        Social: <input type="text" name="soc[]" value="<?php echo $_POST["soc"][$i] ?? ""; ?>"> <br><br>
+    <?php endfor; ?>
+    
+    <input type="submit" name="Submit" value="Submit">
+</form>
+<?php endif; ?>
+
+<?php if (isset($_POST['Submit'])): ?>
+<!-- Step 3: Display Results -->
+<h3>Results:</h3>
 <?php
-// Display Grades if Averages Are Calculated
 if (!empty($averages)) {
-    echo "<h3>Results:</h3>";
     foreach ($averages as $index => $avg) {
         echo "<b>Student " . ($index + 1) . " - Average Marks: $avg | Grade: ";
 
@@ -100,5 +105,7 @@ if (!empty($averages)) {
     }
 }
 ?>
+<?php endif; ?>
+
 </body>
 </html>
